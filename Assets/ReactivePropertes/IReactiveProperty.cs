763ec4711cfdfc9e1462a4463234ebeac0ac1194
Vlados <ptytb2015@mail.ru>
2024-07-19 17:52:49 +0500

    Creat ReactiveProperty

---
+++ Assets/ReactivePropertes/IReactiveProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReactivePropertes
+{
+    public interface IReactiveProperty<T> : IReadOnlyReactiveProperty<T>
+    {
+        public T Value { get; set; }
+    }
+
+    public interface IReadOnlyReactiveProperty<T> : IObservable<T>
+    {
+        public T GetValue();
+    }
+
+    public interface IObservable<T>
+    {
+        public void Subscribe(Action<T> handler);
+        public void SubscribeAndUpdate(Action<T> handler);
+
+        public void UnSubscribe(Action<T> handler);
+    }
+}
