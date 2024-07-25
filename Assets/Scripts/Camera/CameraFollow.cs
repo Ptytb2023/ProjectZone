1c4c187367aed06560e2425cd03c75adad2b9ad5
Vlados <ptytb2015@mail.ru>
2024-07-26 03:24:01 +0500

    CameraFollow Refactoring

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,14 +4,14 @@ namespace Camera
 {
     public class CameraFollow : MonoBehaviour
     {
-        [SerializeField] private Transform target; 
-        [SerializeField] private float smoothSpeed = 50f; 
-        [SerializeField] private Vector3 offset; 
+        [SerializeField] private Transform _target; 
+        [SerializeField] private float _smoothSpeed = 50f; 
+        [SerializeField] private Vector3 _offset; 
 
         private void LateUpdate()
         {
-            Vector3 desiredPosition = target.position + offset;
-            Vector3 smoothedPosition = Vector3.MoveTowards(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            Vector3 desiredPosition = _target.position + _offset;
+            Vector3 smoothedPosition = Vector3.MoveTowards(transform.position, desiredPosition, _smoothSpeed * Time.deltaTime);
 
             transform.position = smoothedPosition;
         }
